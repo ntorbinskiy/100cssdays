5300e9e626984718ea2e4d232147a02f6c78bf26
ntorbinskiy <n.torbinskiy@gmail.com>
2022-09-10 10:48:14 +0300

    da

--- src/components/Foutrh/Fourth.jsx
+++ src/components/Foutrh/Fourth.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./Fourth.css";
 const Fourth = () => {
 	return (
-		<div className="frame frame4">
-			<div className="center center4">
-				<div className="circle small_circle" />
-				<div className="circle mid_circle" />
-				<div className="circle big_circle" />
+		<div class="frame frame4">
+			<div class="center center4">
+				<div class="circle small_circle" />
+				<div class="circle mid_circle" />
+				<div class="circle big_circle" />
 			</div>
 		</div>
 	);

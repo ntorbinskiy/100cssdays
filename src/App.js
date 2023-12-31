6536c3b4679b5c3b1e6c2cd492b30e52a02c8f59
Nikita Torbinskiy <n.torbinskiy@gmail.com>
2023-12-31 18:28:08 +0200

    Minor fixes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import First from './components/First/First';
-import Second from './components/Second/Second';
-import Third from './components/Third/Third';
-import Fourth from './components/Foutrh/Fourth';
-import Fifth from './components/Fifth/Fifth';
-import Sixth from './components/Sixth/Sixth';
-import './App.css';
-import Seventh from './components/Seventh/Seventh';
+import First from "./components/First/First";
+import Second from "./components/Second/Second";
+import Third from "./components/Third/Third";
+import Fourth from "./components/Foutrh/Fourth";
+import Fifth from "./components/Fifth/Fifth";
+import Sixth from "./components/Sixth/Sixth";
+import "./App.css";
+import Seventh from "./components/Seventh/Seventh";
+import { motion } from "framer-motion";
+
 function App() {
 	return (
-		<div className='main'>
-			<div className="animate-charcter">
-				100 CSS DAYS CHALLENGE
-			</div>
+		<motion.div
+			className="main"
+			initial={{ opacity: 0, y: 50 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ ease: "easeOut", duration: 1 }}>
+			<span className="animate-charcter">100 CSS DAYS CHALLENGE</span>
 			<div className="App">
 				<First />
 				<Second />
 				<Third />
-				<Fourth/>
-				<Fifth/>
-				<Sixth/>
-				<Seventh/>
+				<Fourth />
+				<Fifth />
+				<Sixth />
+				<Seventh />
 			</div>
-		</div>
-
+		</motion.div>
 	);
 }
 
--- src/components/First/First.jsx
+++ src/components/First/First.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import "./First.css";
+import { motion } from "framer-motion";
+
 const First = () => {
 	return (
 		<div className="frame frame1">
-			<div className="center1">
-				<div className="numbers">
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ ease: "easeOut", duration: 1 }}
+				className="center1">
+				<motion.div
+					className="numbers"
+					initial={{ y: -100 }}
+					animate={{ y: 0 }}
+					transition={{ duration: 1 }}>
 					<div className="one_0"></div>
 					<div className="one_1"></div>
 					<div className="zero_0"></div>
 					<div className="zero_1"></div>
-				</div>
-				<div className="text">
+				</motion.div>
+				<motion.div
+					className="text"
+					initial={{ y: 100 }}
+					animate={{ y: 0 }}
+					transition={{ duration: 1 }}>
 					<div className="text_days">DAYS</div>CSS CHALLENGE
-				</div>
-			</div>
+				</motion.div>
+			</motion.div>
 		</div>
 	);
 };
